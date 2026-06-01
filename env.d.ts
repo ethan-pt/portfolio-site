1ba45dd84c9c2fe78d1a000ff5fa24fc7e3fce87
ethan-pt <tubbeethan@gmail.com>
2026-06-01 23:42:02 +0000

    update Cloudflare environment variables and add R2_BUCKET_NAME to wrangler configuration

--- env.d.ts
+++ env.d.ts
@@ -1,4 +1,16 @@
 interface CloudflareEnv {
-	DB: D1Database;
-	BUCKET: R2Bucket;
+    DB: D1Database;
+    BUCKET: R2Bucket;
+    NEXTJS_ENV?: string;
+    SITE_ORIGIN?: string;
+    AUTH_COOKIE_SECRET?: string;
+    GITHUB_CLIENT_ID?: string;
+    GITHUB_CLIENT_SECRET?: string;
+    GITHUB_ADMIN_ID?: string;
+    GITHUB_ADMIN_LOGIN?: string;
+    R2_ACCOUNT_ID?: string;
+    R2_ACCESS_KEY_ID?: string;
+    R2_SECRET_ACCESS_KEY?: string;
+    R2_BUCKET_NAME?: string;
+    R2_PUBLIC_BASE_URL?: string;
 }
